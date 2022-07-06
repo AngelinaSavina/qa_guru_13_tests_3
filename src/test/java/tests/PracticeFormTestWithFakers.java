7fb9b6870800bdd20dff1e56ef4554b59f5297fe
angelinasavina <savinaangelina60@gmail.com>
2022-07-06 10:49:24 +0300

    Переменные вынесены в TestData.Доавлена генерация данных с Faker

---
+++ src/test/java/tests/PracticeFormTestWithFakers.java
@@ -0,0 +1,41 @@
+
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class PracticeFormTestWithFakers extends TestBase {
+
+    TestData testData = new TestData();
+
+    @Test
+    void successfulTest() {
+
+
+        practiceFormPage.openPage()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setUserEmail(testData.userEmail)
+                .setGender(testData.gender)
+                .setNumber(testData.phoneNumber)
+                .setDateOfBirth(testData.day, testData.month, testData.year)
+                .chooseSubject(testData.fromSubject)
+                .chooseHobbies(testData.hobby)
+                .uploadPicture(testData.url)
+                .setAddress(testData.currentAddress)
+                .chooseState(testData.state)
+                .chooseCity(testData.city)
+                .pressSubmit()
+                .checkOpenedResultForm()
+                .checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                .checkResult("Student Email", testData.userEmail)
+                .checkResult("Gender", testData.gender)
+                .checkResult("Mobile", testData.phoneNumber)
+                .checkResult("Date of Birth", testData.day + " " + testData.month + "," + testData.year)
+                .checkResult("Subjects", testData.fromSubject)
+                .checkResult("Hobbies", testData.hobby)
+                .checkResult("Picture", "_DSC1044.JPG")
+                .checkResult("Address", testData.currentAddress)
+                .checkResult("State and City", testData.state + " " + testData.city);
+    }
+
+}
